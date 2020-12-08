59c481c9f0d5c11df572038653ee85a52f27c529
faizan7275 <74813450+faizan7275@users.noreply.github.com>
2020-12-08 15:18:05 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,55 +1,45 @@
 $(document).ready(function()
 {
+	$(document).on("click",".check",finishItem);
+	$(document).on("click",".edit",editItem);
 	$("#add").click(function(){
 	var a=$("#ques").val();
 	if(a.length!=0)	
 	{
-	$("#todo").append('<li><input type="checkbox">&emsp;<span class="span1">'+a+'</span>&emsp; <button class="edit">Edit</button><button class="del">Delete</button></li>');
-	$(".edit").click(function(){
-		var b1=$(this).parent();
-		var b3=b1.text();
-		var b2=b3.replace("EditDelete","");
-		console.log(b2);
-		var a1='<br><input type=text value='+b2+'class="edit1" > &emsp; <button class="btn">done</button>'; 
-		$(b1).append(a1);
-		$(".btn").click(function(){
-			console.log($(".edit1").val());
-			$(".span1").text($(".edit1").val());
-			$(".edit1").remove();
-			$(".btn").remove();
-		})
-	})
+	$("#todo").append('<li><input type="checkbox" class="check">&emsp;<span class="span1">'+a+'</span>&emsp; <button class="edit">Edit</button><button class="del">Delete</button></li>');
 	$(".del").click(function(){		
 		var b=$(this).parent();
 		b.remove();		
 	})
-	$("#todo > li > input").click(function(){
-		var z1=$(this).parent();
-		$("#done").append(z1);
-	})
-	$("#todo > li > input").click(function(){
-		if($(this).parent().parent().attr('id')=="todo")
-		{ let x= $(this).parent();
-		console.log(x.text());
-		$("#done").append(x);
-		}
-	})
-	$("#done > li > input").click(function(){
-		if($(this).parent().parent().attr('id')=="done")
-		{ let x= $(this).parent();
-		console.log(x.text());
-		$("#todo").append(x);
-		}
-	})
-	// $("li > input").click(function(){
-	// 	var z1=$(this).parent();
-	// 	if (z1.parent().attr('id')=='todo') {
-	// 	$("#done").append(z1);
-	// }
-	// else {
-	// 	$("#todo").append(z1);
-	// }
-	// })
 	}
 	})
 })
+function finishItem()
+	{
+	if($(this).parent().parent().attr('id')=="done")
+	{ let x= $(this).parent();
+	console.log(x);
+	$("#todo").append(x);
+	}
+	else
+	{
+	let y= $(this).parent();
+	console.log(y);
+	$("#done").append(y);
+	}
+	}
+	function editItem()
+	{
+		var b1=$(this).parent();
+		var b3=b1.text();
+		var b2=b3.replace("EditDelete","");
+		console.log(b2);
+		var a1='&emsp; <input type=text value='+b2+'class="edit1" > &emsp; <button class="btn">done</button>'; 
+		$(b1).append(a1);
+		$(".btn").click(function(){
+			console.log("HII");
+			$(this).parent().children(".span1").text($(".edit1").val());
+			$(".edit1").remove();
+			$(".btn").remove();
+		})
+	}
